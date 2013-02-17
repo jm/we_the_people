0c5ea67099f8ca28772a7d499ff535141d295081
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-02-16 19:36:27 -0500

    Add some spec for the petition and user URL paths being correct.

--- spec/lib/resources_spec.rb
+++ spec/lib/resources_spec.rb
@@ -2,15 +2,32 @@ require 'spec_helper'
 
 describe WeThePeople::Resource, "WeThePeople Generic Resources" do
 
+  def get_constant(resource)
+    "WeThePeople::Resources::#{resource.camelcase}".constantize
+  end
+
   %w{issue location petition response signature user}.each do |resource|
 
-    it "Exists in the WeThePeople::Resource namespace" do
-      "WeThePeople::Resources::#{resource.camelcase}".constantize
+    it "exists in the WeThePeople::Resource namespace" do
+      defined?(get_constant(resource)).should be_true
     end
 
-    it "Can be initialized" do
-      resource_instance = "WeThePeople::Resources::#{resource.camelcase}".constantize.new({})
+    it "can be initialized" do
+      resource_instance = get_constant(resource).new({})
       resource_instance.should_not be_nil
+    end
+
+  end
+
+  %w{petition user}.each do |resource|
+
+    it "can build the correct index URL" do
+      get_constant(resource).build_index_url.should
+      eq "http://petitions.whitehouse.gov/api/v1/#{resource.pluralize}.json"
+    end
+
+    it "generates the correct resource url" do
+      get_constant(resource).path.should eq resource.pluralize
     end
 
   end
